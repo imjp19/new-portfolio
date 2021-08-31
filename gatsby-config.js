84b9ca3aea219a64b5339d46ca1fdf7fd2e082d0
imjp19 <jaypatel19124@gmail.com>
2021-08-31 11:34:38 +0530

    fix: img in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
         title: `Jay Patel`,
         description: `Hello there👋, I am Jay Patel a pre - final year undergrad student at Sardar Vallabhbhai Patel Institute of Technology.I am a full - stack web developer.I am exploring various technologies in Web Development.And learning new technologies on a required basis.I have a simple motto.Pick a project definition and then explore the definition.After exploring decide which technologies will be used to make the way of user experience smooth.If I know the technology which will be used in developing.Else I learn required technologies and implement them directly on the project.And at the end of the completing project, I had lots of new skills / concepts which I never used or never had.Currently I am working as a Web Developer at my own projects and sometimes on freelance projects just for exploring and learning new things about client requirements.Also, I am Data Science, Machine Learning, Artificial Intelligence enthusiast.`,
         author: `@imjp.dev`,
+        image: `src/images/jay.png`
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
