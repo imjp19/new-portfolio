bb1dedc02d867c3324e681d8fa5a0127fc083ca1
imjp19 <jaypatel19124@gmail.com>
2021-08-31 11:30:42 +0530

    fix: desc in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
         title: `Jay Patel`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        description: `Hello there👋, I am Jay Patel a pre - final year undergrad student at Sardar Vallabhbhai Patel Institute of Technology.I am a full - stack web developer.I am exploring various technologies in Web Development.And learning new technologies on a required basis.I have a simple motto.Pick a project definition and then explore the definition.After exploring decide which technologies will be used to make the way of user experience smooth.If I know the technology which will be used in developing.Else I learn required technologies and implement them directly on the project.And at the end of the completing project, I had lots of new skills / concepts which I never used or never had.Currently I am working as a Web Developer at my own projects and sometimes on freelance projects just for exploring and learning new things about client requirements.Also, I am Data Science, Machine Learning, Artificial Intelligence enthusiast.`,
+        author: `@imjp.dev`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
